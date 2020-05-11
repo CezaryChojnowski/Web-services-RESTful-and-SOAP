6742870d30a52fc4907dd81dc9fdd3028d3fe109
Cezary Chojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-05-11 20:26:47 +0200

    Second exercise

--- src/main/java/com/rsi/demo/domain/MessageService.java
+++ src/main/java/com/rsi/demo/domain/MessageService.java
@@ -3,19 +3,42 @@ package com.rsi.demo.domain;
 import com.rsi.demo.model.Message;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class MessageService {
 
-    public List<Message> generateMessagesList(){
+    static private Map<Long, Message> messageMap = new HashMap<>();
 
-        List<Message> messageList = new ArrayList<>();
-        messageList.add(new Message(1L, "Pierwsza wiadomość", new Date(),"User1"));
-        messageList.add(new Message(1L, "Druga wiadomość", new Date(),"User2"));
-        messageList.add(new Message(1L, "Trzecia wiadomość", new Date(),"User3"));
-        return messageList;
+    public MessageService(){
+        messageMap.put(1L, new Message(1L, "Pierwsza wiadomość", new Date(),"User1"));
+        messageMap.put(2L, new Message(2L, "Druga wiadomość", new Date(),"User2"));
+        messageMap.put(3L, new Message(3L, "Trzecia wiadomość", new Date(),"User3"));
+    }
+
+    public List<Message> getAllMessages(){
+        return new ArrayList<>(messageMap.values());
+    }
+
+    public Message getMessage(Long id){
+        return messageMap.get(id);
+    }
+
+    public Message createMessage(Message message){
+        message.setId(messageMap.size() + 1L);
+        messageMap.put(messageMap.size() + 1L, message);
+        return message;
+    }
+
+    public Message updateMessage(Long id, Message message){
+        messageMap.get(id).setId(message.getId());
+        messageMap.get(id).setAuthor(message.getAuthor());
+        messageMap.get(id).setCreated(message.getCreated());
+        messageMap.get(id).setMessage(message.getMessage());
+        return message;
+    }
+
+    public Message deleteMessage(Long id){
+       return messageMap.remove(id);
     }
 }
--- src/main/java/com/rsi/demo/rest/controller/MessageController.java
+++ src/main/java/com/rsi/demo/rest/controller/MessageController.java
@@ -4,24 +4,48 @@ import com.rsi.demo.domain.MessageService;
 import com.rsi.demo.model.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
 
+//    @Context
+//    private HttpHeaders httpHeaders;
+
     private final MessageService messageService;
 
-    @GetMapping(produces = {
+    @GetMapping(value = "{messageId}",
+            produces = {
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_JSON_VALUE
     })
-    public List<Message> getText(){
-        return messageService.generateMessagesList();
+    public Message getMessage(@PathVariable("messageId") Long id){
+        return messageService.getMessage(id);
+    }
+
+    @GetMapping
+    public List<Message> getAllMessages(){
+        return messageService.getAllMessages();
+    }
+
+    @PostMapping
+    public Message createNewMessage(@RequestBody Message message){
+        return messageService.createMessage(message);
+    }
+
+    @PutMapping(value = "/{messageId}")
+    public Message updateMessage(@PathVariable("messageId") Long id,
+                                 @RequestBody Message message){
+        return messageService.updateMessage(id, message);
+    }
+
+    @DeleteMapping(value = "/{messageId}")
+    public Message removeMessage(@PathVariable("messageId") Long id){
+        return messageService.deleteMessage(id);
     }
 }
